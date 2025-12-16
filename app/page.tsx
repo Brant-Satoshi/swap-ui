ffe70c6b71609f80bf24ef9a3e0ef499af4497ef
Brant-Satoshi <1351805238@qq.com>
2025-12-16 23:03:16 +0800

    feat: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="p-8 space-y-4">
-          <h1 className="text-2xl font-semibold">Coinbase Blue Theme</h1>
 
-          <button className="bg-primary text-primary-foreground px-4 py-2 rounded-lg">
-            Primary
-          </button>
-
-          <button className="bg-secondary text-secondary-foreground px-4 py-2 rounded-lg">
-            Secondary
-          </button>
         </div>
       </main>
     </div>
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { Button } from "@/components/ui/button"
+
+export default function Navbar() {
+    const { theme, setTheme } = useTheme();
+
+    const next = theme === "dark" ? "light" : "dark";
+    return <nav className="w-full h-16 bg-gray-800 text-white flex items-center px-4">
+        <Image src="/logo.png" alt="Logo" width={200} height={50} />
+
+        <div className="ml-8 space-x-4 flex items-center justify-end flex-grow">
+            <HoverCard>
+                <HoverCardTrigger asChild>
+                    <Button variant="ghost" className="ml-4 px-4 py-2 rounded cursor-pointer" >
+                        <Image src="/language.png" alt="Logo" width={20} height={20} />
+                    </Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-40">
+                    <div className="flex flex-col space-y-2 cursor-pointer hover:text-primary">English</div>
+                    <div className="flex mt-4 flex-col space-y-2 cursor-pointer hover:text-primary">简体中文</div>
+                </HoverCardContent>
+            </HoverCard>
+
+            <Button
+                className="bg-gray-700 px-4 py-2 rounded cursor-pointer"
+                onClick={() => setTheme(next)}
+            >
+                <Image src="/logo.svg" alt="Logo" width={30} height={30} />
+            </Button>
+        </div>
+    </nav>;
+}
